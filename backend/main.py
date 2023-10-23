7ab523e9ae26b44c2bb3e9f24ea7307a91138fe6
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2023-10-23 18:15:49 +0300

    Initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+db = {"posts": []}
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return {"status": 200}
+    if request.method == "GET":
+        return db["posts"]
+    if request.method == "POST":
+        db["posts"].append(request.get_json())
+        return db["posts"]
 
 
 if __name__ == "__main__":
